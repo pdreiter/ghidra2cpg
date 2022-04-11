08304043907583c308d82a9d51acf2cc0ac31dbc
pdreiter <pdreiter@asu.edu>
2022-04-11 14:15:09 -0700

    eliminating need to be in local ghidra2cpg directory

--- ghidra2cpg.sh
+++ ghidra2cpg.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-ghidra2cpg/target/universal/stage/bin/ghidra2cpg -J-Dlog4j.configurationFile=config/log4j-ghidra.xml "$@"
+SCRIPTDIR=$(dirname -- $(realpath -- ${BASH_SOURCE[0]}))
+$SCRIPTDIR/ghidra2cpg/target/universal/stage/bin/ghidra2cpg -J-Dlog4j.configurationFile=config/log4j-ghidra.xml "$@"
